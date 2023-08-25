2831016289a060418df87cad50c4c57088c510e1
juliamelian <mariajuliamelian@gmail.com>
2023-08-24 21:09:04 -0300

    correcciones
    - pasé código repetido a una función llamada agregarALista que después invoqué cuando fue necesario
    - corregí los comentarios para que quedara más prolijo

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,31 +1,41 @@
-// con este addeventlistener hacemos que el script espere a que la pagina se cargue para ejecutarse
+// con este addEventListener hacemos que el script espere a que la página se cargue para ejecutarse
 document.addEventListener('DOMContentLoaded', function () {
-    /* una vez que se cargo la pagina, 
-    creamos una variable por cada uno de los elementos 
-    importantes que queremos manipular del html  para poder acceder a ellos */
+    
+    /* Traer los botones:
+    una vez que se cargo la página, creamos una variable por cada uno de los elementos 
+    importantes que queremos manipular del HTML para poder acceder a ellos */
     const itemInput = document.getElementById('item');
     const addButton = document.getElementById('agregar');
     const clearButton = document.getElementById('limpiar');
     const contenedor = document.getElementById('contenedor');
 
-
-    // Cargar elementos almacenados al inicio 
-    // en esta constante se traen los items almacenados previamente en el local storage, si no hay se inicializa 
-    // como un arreglo vacio
-    const storedItems = JSON.parse(localStorage.getItem('items')) || [];
-    // con este foreach recorremos el array de items y creamos un 
-    // li dentro del contenedor de la lista para mostrar cada uno de ellos
-    storedItems.forEach(item => {
+    /* Cargar elementos almacenados al inicio:
+    en esta constante se traen los ítems almacenados previamente en el local storage, 
+    si no hay nada se inicializa como un arreglo vacio */ 
+    const storedItems = JSON.parse(localStorage.getItem('items')) || []; // esta es la forma de inicializar porque si esta vacío da error, evita el error
+    
+    /* Función para agregar elementos a la lista: 
+    con esta función creamos los elementos que queremos agregar
+    a la lista y los añadimos al elemento del HTML correspondiente(contenedor)*/
+    function agregarALista (item){
         const li = document.createElement('li');
         li.className = 'list-group-item';
         li.appendChild(document.createTextNode(item));
-        contenedor.appendChild(li);
+        contenedor.appendChild(li)
+    };
+
+    /* Agregar elementos del local storage: 
+    con este forEach recorremos el local storage y usamos
+    la función agregarALista para añadir los elementos que haya a la lista */
+    storedItems.forEach(item => {
+        agregarALista(item)
     });
 
-    /* Cuando se hace clic en el botón "Agregar", obtenemos el valor del campo de entrada
+    /* Agregar elementos ingresados: 
+    con este segmento del código, obtenemos el valor del campo de entrada al hacer clic en el botón "Agregar",
     y lo limpiamos de espacios en blanco al principio y al final. Si el valor no está vacío, 
     agregamos el nuevo ítem al arreglo storedItems y lo almacenamos en el almacenamiento local.
-    También creamos un elemento <li> para el nuevo ítem y lo agregamos al contenedor de la lista. 
+    Usamos la función AgregarALista para añadir los nuevos ítems al contenedor.
     Finalmente, limpiamos el campo de entrada. */
     addButton.addEventListener('click', function () {
         const newItem = itemInput.value.trim(); // el trim saca los espacios del texto ingresado en itemInput
@@ -33,18 +43,15 @@ document.addEventListener('DOMContentLoaded', function () {
             // Agregar el nuevo ítem al almacenamiento local y a la vista
             storedItems.push(newItem);
             localStorage.setItem('items', JSON.stringify(storedItems));
-
-            const li = document.createElement('li');
-            li.className = 'list-group-item';
-            li.appendChild(document.createTextNode(newItem));
-            contenedor.appendChild(li);
-
+            agregarALista(newItem);
             itemInput.value = ''; // Limpiar el campo de entrada
         }
     });
 
-    /* Cuando se hace clic en el botón "Limpiar", eliminamos los ítems almacenados del almacenamiento
-     local y vaciamos el contenido del contenedor de la lista */
+    /* Limpiar contenedor: 
+    con este addEventListener, hacemos que cuando se registre un clic en el botón "Limpiar", 
+    los ítems almacenados en el local storage sean eliminados
+    y vaciamos el contenido del contenedor de la lista */
     clearButton.addEventListener('click', function () {
         // Eliminar el listado almacenado y actualizar la vista
         localStorage.removeItem('items');
